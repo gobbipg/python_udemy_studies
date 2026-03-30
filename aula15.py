28da07fe69e838df096c9d9c9cd628be1e585e3f
gobbipg <paulogj.gobbip@gmail.com>
2026-03-30 01:34:09 -0300

    Entrando em coletas de dados com input

---
+++ aula15.py
@@ -0,0 +1,10 @@
+# nome = input('qual o seu nome? ')
+# print(f'o seu nome é {nome=}')
+
+numero_1 = input('digite um número: ')
+numero_2 = input('digite outro número: ')
+
+int_numero_1 = int(numero_1)
+int_numero_2 = int(numero_2)
+
+print(f'a soma dos números é: {int_numero_1 + int_numero_2}')
