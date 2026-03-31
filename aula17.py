8dbff8939ce759079649897ebe7fbf969403b954
gobbipg <paulogj.gobbip@gmail.com>
2026-03-31 00:10:51 -0300

    operadores de comparação (relacionais)

---
+++ aula17.py
@@ -0,0 +1,28 @@
+# if / elif      / else
+# se / se não se / se não
+
+condicao1 = False
+condicao2 = False
+condicao3 = True
+condicao4 = False
+
+if condicao1:
+    print('código da condição 1')
+    print('código da condição 1')
+
+elif condicao2:
+    print('código da condição 2')
+
+elif condicao3:
+    print('código da condição 3')
+
+elif condicao4:
+    print('código da condição 4')
+
+else:
+    print('nenhuma das condições...')
+
+if 10 == 10:
+    print('outro if')
+
+print('fora do if')
