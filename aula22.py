115b8659eaf0be2fb78a1a0567503109b5f89d13
gobbipg <paulogj.gobbip@gmail.com>
2026-04-02 23:50:50 -0300

    Continuação...

---
+++ aula22.py
@@ -0,0 +1,27 @@
+# operadores lógicos 
+# and (e) or (ou) not (não)
+# and - todas as condições precisam ser verdadeiras
+# se qualquer valor for considerado false, a expressão inteira será avaliada naquele valor.
+# são consideradas falsy (que vc já viu)
+# 0, 0.0, '', False
+# também existe o tipo None que é 
+# usado para representar um não valor.
+
+# entrada = input('[E]ntrar ou [S]air: ')
+# senha = input('Digite a senha: ')
+# senha_permitida = '12345'
+
+# if (entrada == 'E' or entrada == 'e') and senha == senha_permitida:
+#    print('Você está logado!')
+
+# else:
+#    print('Você saiu!')
+
+# avaliação de curto circuito
+# print(True and True and True) # True
+# print(True and False and True) # False
+# print(True and 0 and True) # 0 False
+# print(bool('')) # False
+
+senha = input('Senha: ') or 'sem senha'
+print(senha)
---
+++ aula24.py
@@ -0,0 +1,5 @@
+# operadores in e not in
+# strings são iteráveis
+# 0 1 2 3 4 5
+# o t á v i o
+#-6-5-4-3-2-1
