30a324d8579a7b362d2e2bd3a0865151e4515b64
gobbipg <paulogj.gobbip@gmail.com>
2026-04-22 14:50:50 -0300

    Calculadora com while

--- aula40.py
+++ aula40.py
@@ -1,7 +1,47 @@
 """ Calculadora com while """
 while True:
+    numero_1 = input('Digite o primeiro número: ')
+    numero_2 = input('Digite o segundo número: ')
+    operador = input('Digite o operador [+-/*]: ')
 
-    ...
+    numeros_validos = None
+    num_1_float = 0
+    num_2_float = 0
+
+    try:
+        num_1_float = float(numero_1)
+        num_2_float = float(numero_2)
+        numeros_validos = True
+    except:
+        numeros_validos = None
+
+    if numeros_validos is None:
+        print('Um ou ambos os números digitados são inválidos.')
+        continue
+
+    operadores_permitidos = '+-/*'
+
+    if operador not in operadores_permitidos:
+        print('Operador inválido.')
+        continue
+
+    if len(operador) > 1:
+        print('Digite apenas um operador.')
+        continue
+
+    print('Realizando sua conta. Confira o resultado abaixo:')
+
+    if operador == '+':
+        print(f'{num_1_float} + {num_2_float} =', num_1_float + num_2_float)
+    elif operador == '-':
+        print(f'{num_1_float} - {num_2_float} =', num_1_float - num_2_float)
+    elif operador == '/':
+        print(f'{num_1_float} / {num_2_float} =', num_1_float / num_2_float)
+    elif operador == '*':
+        print(f'{num_1_float} * {num_2_float} =', num_1_float * num_2_float)
+    else:
+        print('Nunca deveria chegar aqui.')
+
 
     sair = input('Deseja sair? Digite [s]: ').lower().startswith('s')
     # lower: deixa toda a string com letras minusculas (mesmo se o usuario digitar com maiusculas).
