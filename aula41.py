4f931b764b6c878240dea2ed4a860cc84f435241
gobbipg <paulogj.gobbip@gmail.com>
2026-04-24 16:42:23 -0300

    Finalizando while e iniciando for

---
+++ aula41.py
@@ -0,0 +1,16 @@
+""" while/else"""
+string = 'Valorqualquer'
+
+i = 0
+while i < len(string):
+    letra = string[i]
+
+    if letra == ' ':
+        break
+
+    print(letra)
+    i += 1
+else:
+    print('Não encontrei espaços.')
+
+print('Fora do laço.')
