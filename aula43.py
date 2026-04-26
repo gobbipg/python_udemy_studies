c46456294c3f5d67dace6337f766f21b947c7bc5
gobbipg <paulogj.gobbip@gmail.com>
2026-04-26 14:57:29 -0300

    Iniciando e entendendo o for

--- aula43.py
+++ aula43.py
@@ -1,1 +1,18 @@
+# Se tem lista ou uma sequência, o melhor é o for
+# Se tem condição aberta em que você não sabe quando vai acabar, o melhor é o while
+
+# texto = 'Python'
+# i = 0
+
+# while i < len(texto):
+#     print(texto[i], i)
+
+#     i += 1
+
 texto = 'Python'
+novo_texto = ''
+
+for letra in texto:
+    novo_texto += f'*{letra}'
+    print(letra)
+print(novo_texto)
---
+++ aula44.py
@@ -0,0 +1,9 @@
+"""
+For + Range
+range -> range(Start, stop, step)
+"""
+
+numeros = range(0, 100, 8)
+
+for numero in numeros:
+    print(numero)
