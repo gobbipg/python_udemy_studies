9466264139be7970d8412f91a9529ec96ca46440
gobbipg <paulogj.gobbip@gmail.com>
2026-05-13 23:24:30 -0300

    Entendendo as listas

--- aula49.py
+++ aula49.py
@@ -3,7 +3,20 @@ Listas em Python
 Tipo list - Mutável
 Suporta vários valores de qualquer tipo
 Conhecimentos reutilizáveis - índices e fatiamento
-Méetodos úteis: append, insert, pop, del, clear, extend, +
+Métodos úteis:
+    append, insert, pop, del, clear, extend, +
+Create Read Update   Delete
+Criar, ler, alterar, apagar = lista[i] (CRUD)
 """
-lista = []
-print(lista)
+#        0   1   2   3   4   5
+lista = [10, 20, 30, 40]
+# lista[2] = 300
+# del lista[2]
+# print(lista)
+# print(lista[2])
+lista.append(50)
+lista.pop()
+lista.append(60)
+lista.append('bbb')
+ultimo_valor = lista.pop(3)
+print(lista, 'removido:', ultimo_valor)
